65eae35530c5144dae79bf3c8e862c7c00f65a89
nxf83910 <balaramasamymc.x@nxp.com>
2022-09-21 12:36:59 +0530

    [artf977706]: [EMVCO] Processing the messages in Handler
    Handling the incoming messages from client in background thread

--- emvco/com/nxp/emvco/NxpNfcDiscoveryProfile.java
+++ emvco/com/nxp/emvco/NxpNfcDiscoveryProfile.java
@@ -18,10 +18,17 @@
 package com.nxp.emvco;
 
 import android.annotation.RequiresPermission;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.INfcAdapter;
 import android.nfc.NfcAdapter;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.ParcelFormatException;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -35,14 +42,24 @@ import java.io.IOException;
 import java.util.List;
 
 public final class NxpNfcDiscoveryProfile {
+  private static final String TAG = NxpNfcDiscoveryProfile.class.getName();
   private IEMVCoAppClientCallback mEMVCoAppClientCallback = null;
-  private static INfcAdapter mNfcAdapter;
-  private static INxpNfcDiscoveryProfile sNxpDiscoveryService;
+  private INxpNfcDiscoveryProfile sNxpDiscoveryService;
+  private Object mNfcProfileSyncObj = new Object();
+  private int mNfcState = NfcAdapter.STATE_OFF;
+  private INfcAdapter mNfcAdapter;
+  private static final int MSG_REGISTER_EMVCO_LISTENER = 1;
+  private static final int MSG_SET_EMVCO_MODE = 2;
+  private NxpNfcDiscoveryProfileHandler mHandler;
+  private static NxpNfcDiscoveryProfile mNxpNfcDiscoveryProfile;
+
   private IEMVCoHalClientCallback.Stub mEmvcoHalCallback =
       new IEMVCoHalClientCallback.Stub() {
         @Override
         public void sendData(byte[] data) {
+          Log.e(TAG, "sendData ");
           if (mEMVCoAppClientCallback != null) {
+            Log.e(TAG, "sendData propogated to app");
             mEMVCoAppClientCallback.sendData(data);
           } else {
             Log.d(TAG, "sendData is NULL");
@@ -50,19 +67,25 @@ public final class NxpNfcDiscoveryProfile {
         }
         @Override
         public void sendEvent(EmvcoEvent event, EmvcoStatus status) {
+          Log.e(TAG, "sendEvent ");
           if (mEMVCoAppClientCallback != null) {
+            Log.e(TAG, "sendEvent propogated to app");
             mEMVCoAppClientCallback.sendEvent(event, status);
           } else {
             Log.d(TAG, "sendEvent is NULL");
           }
         }
       };
-  private static final String TAG = NxpNfcDiscoveryProfile.class.getName();
-  private static NxpNfcDiscoveryProfile mNxpNfcDiscoveryProfile;
 
   private NxpNfcDiscoveryProfile() {
+    Log.e(TAG, "NxpNfcDiscoveryProfile");
+    mHandler = new NxpNfcDiscoveryProfileHandler();
     mNfcAdapter = getServiceInterface();
     sNxpDiscoveryService = getNxpNfcDiscoveryProfileAdapterInterface();
+
+    Message emvcoMsg = mHandler.obtainMessage();
+    emvcoMsg.what = MSG_REGISTER_EMVCO_LISTENER;
+    mHandler.sendMessage(emvcoMsg);
   }
 
   public static synchronized NxpNfcDiscoveryProfile getInstance() {
@@ -74,14 +97,11 @@ public final class NxpNfcDiscoveryProfile {
 
   @RequiresPermission(android.Manifest.permission.NFC)
   public void doSetEMVCoMode(int tech, boolean isStartEMVCo) {
-    try {
-      Log.i(TAG, "doSetEMVCoMode ");
-      sNxpDiscoveryService.doSetEMVCoMode(tech, isStartEMVCo);
-      return;
-    } catch (RemoteException e) {
-      e.printStackTrace();
-      return;
-    }
+    Message msg = mHandler.obtainMessage();
+    msg.what = MSG_SET_EMVCO_MODE;
+    msg.arg1 = tech;
+    msg.arg2 = isStartEMVCo ? 1 : 0;
+    mHandler.sendMessage(msg);
   }
 
   @RequiresPermission(android.Manifest.permission.NFC)
@@ -98,23 +118,15 @@ public final class NxpNfcDiscoveryProfile {
   @RequiresPermission(android.Manifest.permission.NFC)
   public void
   doRegisterEMVCoEventListener(IEMVCoAppClientCallback mEMVCoCallback) {
-    try {
-      Log.i(TAG, "doRegisterEMVCoEventListener ");
-      if (mEMVCoCallback != null) {
-        mEMVCoAppClientCallback = mEMVCoCallback;
-      } else {
-        Log.e(TAG, "App has not registered callback");
-      }
-      sNxpDiscoveryService.doRegisterEMVCoEventListener(mEmvcoHalCallback);
-      return;
-    } catch (RemoteException e) {
-      e.printStackTrace();
-      return;
+    if (mEMVCoCallback != null) {
+      mEMVCoAppClientCallback = mEMVCoCallback;
+    } else {
+      Log.e(TAG, "App has not registered callback");
     }
   }
 
   /** get handle to NFC service interface */
-  private static INfcAdapter getServiceInterface() {
+  private INfcAdapter getServiceInterface() {
     /* get a handle to NFC service */
     IBinder b = ServiceManager.getService("nfc");
     if (b == null) {
@@ -123,8 +135,7 @@ public final class NxpNfcDiscoveryProfile {
     return INfcAdapter.Stub.asInterface(b);
   }
 
-  private static INxpNfcDiscoveryProfile
-  getNxpNfcDiscoveryProfileAdapterInterface() {
+  private INxpNfcDiscoveryProfile getNxpNfcDiscoveryProfileAdapterInterface() {
     if (mNfcAdapter == null) {
       throw new UnsupportedOperationException(
           "You need a reference from NfcAdapter to use the "
@@ -139,6 +150,45 @@ public final class NxpNfcDiscoveryProfile {
       return INxpNfcDiscoveryProfile.Stub.asInterface(b);
     } catch (RemoteException e) {
       return null;
+    }
+  }
+
+  final class NxpNfcDiscoveryProfileHandler extends Handler {
+    @Override
+    public void handleMessage(Message msg) {
+      switch (msg.what) {
+      case MSG_SET_EMVCO_MODE: {
+        int technologyToPool = msg.arg1;
+        Log.i(TAG, "doSetEMVCoMode technologyToPool:" + technologyToPool);
+        boolean isStartEMVCo = (msg.arg2 == 1) ? true : false;
+        try {
+          Log.i(
+              TAG,
+              "doSetEMVCoMode NFC oFF so call doSetEmVCo mode with technologyToPool:" +
+                  technologyToPool + " isStartEMVCo:" + isStartEMVCo);
+          sNxpDiscoveryService.doSetEMVCoMode(technologyToPool, isStartEMVCo);
+        } catch (RemoteException e) {
+          e.printStackTrace();
+        }
+        break;
+      }
+      case MSG_REGISTER_EMVCO_LISTENER: {
+        IEMVCoAppClientCallback mEMVCoCallback =
+            (IEMVCoAppClientCallback)msg.obj;
+        try {
+          Log.i(TAG, "doRegisterEMVCoEventListener ");
+          if (mEMVCoCallback != null) {
+            mEMVCoAppClientCallback = mEMVCoCallback;
+          } else {
+            Log.e(TAG, "App has not registered callback");
+          }
+          sNxpDiscoveryService.doRegisterEMVCoEventListener(mEmvcoHalCallback);
+        } catch (RemoteException e) {
+          e.printStackTrace();
+        }
+        break;
+      }
+      }
     }
   }
 }
