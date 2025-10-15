2ed7279a4869bff39307f3e2d9b3ebea60d9af54
nxg08255 <venkatajahnavi.dubisetty@nxp.com>
2025-10-15 14:58:35 +0530

    Copyright update.
        - Year update as changes done in the file.

--- emvco/com/nxp/emvco/NxpProfileDiscovery.java
+++ emvco/com/nxp/emvco/NxpProfileDiscovery.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2022-2023 NXP
+ *  Copyright 2022-2023,2025 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
